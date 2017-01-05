63d965c39281cc6832844a07da32f47a49ab5aa5
randomvs <victorearellano@gmail.com>
2017-01-05 05:49:35 +0000

    change result for 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 print("Last digit of {:d} is ".format(number), end="")
+
 if number < 0:
     last = number % -10
 else:
@@ -10,6 +11,6 @@ else:
 if last > 5:
     print("{:d} and is greater than 5".format(last))
 elif last == 0:
-    print("0 and is 0")
+    print("{:d} and is 0".format(last))
 else:
     print("{:d} and is less than 6 and not 0".format(last))
