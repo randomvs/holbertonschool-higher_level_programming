d116ce5f092e6e1c5d282ac0b2b1f6464893fc70
randomvs <victorearellano@gmail.com>
2017-02-12 20:02:53 +0000

    q2 corrected outputs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,13 @@
 if __name__ == "__main__":
     import sys
 
-    print("{:d} arguments:".format(len(sys.argv) - 1))
+    print("{:d} argument".format(len(sys.argv) - 1), end="")
+    if len(sys.argv) == 1:
+        print("s.")
+    elif len(sys.argv) == 2:
+        print(":")
+    else:
+        print("s:")
 
     for i in range(len(sys.argv) - 1):
         print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
