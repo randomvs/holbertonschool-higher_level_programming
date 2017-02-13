ae2ea3be90c91e475007a55de5e534073d159a5d
randomvs <victorearellano@gmail.com>
2017-02-13 01:01:23 +0000

    q4 edge case correction for idx range

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx >= len(my_list):
+    if idx >= len(my_list) or idx < 0:
         return my_list.copy
 
     new_list = my_list.copy()
