b8a51665ed76b3be672dcc2b49ae9f8eb145ff99
randomvs <victorearellano@gmail.com>
2017-02-15 05:44:02 +0000

    q11 pep8 correction

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -2,9 +2,8 @@
 def mul(x, y):
     return x * y
 
+
 def mutiply_list_map(my_list=[], number=0):
-    if len(my_list) is 0:
-        return
 
     new_list = list(map(lambda x: x * number, my_list))
 
