15395a5fb08f743790be5aa3cd1081a190972793
randomvs <victorearellano@gmail.com>
2017-02-15 03:37:22 +0000

    q4 remove extra space

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,7 +4,7 @@ def only_diff_elements(set_1, set_2):
     new_set = []
 
     for i in range(len(temp_list)):
-        if temp_list.count(temp_list[i]) is  1:
+        if temp_list.count(temp_list[i]) is 1:
             new_set.append(temp_list[i])
 
     return set(new_set)
